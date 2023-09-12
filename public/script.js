7fccc3843b12282a20d71d6c9c70cf591a2575e8
purplena <=>
2023-09-12 20:04:21 +0200

    page of details is ready with all js/jQuery interated

--- public/script.js
+++ public/script.js
@@ -41,34 +41,39 @@ fetch('countries.json')
   })
   .catch((error) => console.error('Error:', error));
 
+document.getElementById('number_nights').innerText = '0 night(s)';
+
 //Here I initialize y Bootstrap DatePicker Plugin
 // I block all the dates before today and I highlight today
 $(document).ready(function () {
-  $('.datepicker')
-    .datepicker({
-      format: 'dd/mm/yyyy',
-      startDate: new Date(),
-      todayHighlight: true,
-    })
-    .on('changeDate', function (e) {
-      // console.log(e);
-      var startDateString = $('#date_start').datepicker('getDate');
-      var endDateString = $('#date_finish').datepicker('getDate');
+  $('.datepicker').datepicker({
+    format: 'dd/mm/yyyy',
+    startDate: new Date(),
+    todayHighlight: true,
+  });
+  $('#date_finish').on('changeDate', function (e) {
+    var startDateString = $('#date_start').datepicker('getDate');
+    var endDateString = $('#date_finish').datepicker('getDate');
 
-      // Step 2: Convert the date strings to Date objects
-      var startDate = new Date(startDateString);
-      var endDate = new Date(endDateString);
+    // Step 2: Convert the date strings to Date objects
+    var startDate = new Date(startDateString);
+    var endDate = new Date(endDateString);
 
-      // Step 3: Calculate the difference in milliseconds
-      var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+    // Step 3: Calculate the difference in milliseconds
+    var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
 
-      // Step 4: Convert the difference to the number of nights
-      var numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    // Step 4: Convert the difference to the number of nights
+    var numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-      // Step 5: Display the number of nights
-      // $('#number_of_nights').text(numberOfNights + ' nights');
-      console.log(numberOfNights);
-    });
+    // Step 5: Display the number of nights
+    // $('#number_of_nights').text(numberOfNights + ' nights');
+    document.getElementById('number_nights').innerText =
+      numberOfNights + ' night(s)';
+
+    var price = document.getElementById('price-span').innerText;
+    var total = price * numberOfNights;
+    document.getElementById('total').innerText = total + ' €';
+  });
 });
 
 // console.log($('.datepicker').datepicker('getStartDate'));
